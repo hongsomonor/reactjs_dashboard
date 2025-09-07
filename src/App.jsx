b937f3cef0c1872a085e9bffd34f3f99127a58d0
hongsomonor <somonorhong011@gmail.com>
2025-09-07 22:37:17 +0700

    multiple layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/auth/LoginPage"
 import RegisterPage from "./pages/auth/RegisterPage"
 import PageNotFound from "./pages/404/PageNotFound"
 import MainLayout from "./components/layout/MainLayout"
+import MainLayoutLogin from "./components/layout/MainLayoutLogin"
 
 
 function App() {
@@ -12,13 +13,20 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route element={<MainLayout/>}> {/* click smth in main layout to redirect to any route */}
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<PageNotFound />} /> {/* if page not found */}
+        </Route>
+
+        {/* when user click login or logout, route redirect to layout of Login and Register. and leave of main page */}
+        <Route element={<MainLayoutLogin/>}> {/* click smth in main layout to redirect to any route */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="*" element={<PageNotFound />} /> {/* if page not found */}
         </Route>
+
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/layout/MainLayoutLogin.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Outlet , Link } from 'react-router-dom'
+
+const MainLayoutLogin = () => {
+  return (
+    <div>
+        <h1 className='text-5xl font-bold text-sky-600 mt-7'>Auth</h1>
+        <div className='text-4xl font-bold text-gray-600 mt-5 ml-20 cursor-pointer flex gap-4'>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+        </div>
+        <div>
+            <Outlet/>
+        </div>
+    </div>
+  )
+}
+
+export default MainLayoutLogin
